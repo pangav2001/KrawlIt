e518e94fea721c0dd08983a7db8e1785f4537b7e
Panayiotis Gavriil <panayiotis.gavriil19@imperial.ac.uk>
2020-10-18 22:21:43 +0100

    Minor changes to variable naming and code style

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,25 +1,23 @@
 import org.jsoup.Jsoup
 import java.io.IOException
 
-fun krawlURL(_URL: String, _links: HashSet<String>, _desNum: Int)
+fun krawlURL(url: String, links: HashSet<String>, desNum: Int)
 {
-    if (!_links.contains(_URL) && _links.size < _desNum && _URL != "")
+    if (!links.contains(url) && links.size < desNum && url != "")
     {
         try
         {
-            _links.add(_URL)
-            val size = _links.size
-            println("$size $_URL")
-
-            val page = Jsoup.connect(_URL).get()
-
+            links.add(url)
+            val size = links.size
+            println("$size $url")
+            val page = Jsoup.connect(url).get()
             val URLs = page.select("a[href]")
-
-            for (html in URLs) krawlURL(html.attr("abs:href"), _links, _desNum)
-        } catch (e: IOException)
+            for (html in URLs) krawlURL(html.attr("abs:href"), links, desNum)
+        }
+        catch (e: IOException)
         {
             val error = e.message
-            System.err.println("Got: '$error' for '$_URL'")
+            System.err.println("Got: '$error' for '$url'")
         }
     }
 }
@@ -27,9 +25,9 @@ fun krawlURL(_URL: String, _links: HashSet<String>, _desNum: Int)
 fun main(args: Array<String>)
 {
     println("Please enter the URL you want to crawl through: ")
-    val URL: String = readLine()!!
+    val url: String = readLine()!!
     println("Please enter desired number of URLs: ")
     val desNum: Int = readLine()!!.toInt()
     val links: HashSet<String> = HashSet()
-    krawlURL(URL, links, desNum)
+    krawlURL(url, links, desNum)
 }
